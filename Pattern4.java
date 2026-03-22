0b6451288addace98a18ef1a074e28c5f7f8e1c0
Felcia A <jeyakumararul91@gmail.com>
2026-03-22 22:18:06 +0530

    Add files via upload

---
+++ Pattern4.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Pattern4 {
+    public static void main(String[]args){
+        Scanner s=new Scanner(System.in);
+        int r=s.nextInt();
+        int c=s.nextInt();
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                if((i==0&&j<c-2)||(i==r-1&&j<c-2)){
+                    System.out.print("*");
+                }
+               else if((j==0&&j<r-2)||(j==c-1&&i>0&&i<r-1))
+               System.out.print("*");
+               else
+               System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
