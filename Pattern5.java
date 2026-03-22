92293e012ea47a7431603a3bd270343d46a73980
Felcia A <jeyakumararul91@gmail.com>
2026-03-22 22:57:48 +0530

    Add files via upload

---
+++ Pattern5.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Pattern5 {
+    public static void main(String[]args){
+        Scanner s=new Scanner(System.in);
+        int r=s.nextInt();
+        for(int i=0;i<r;i++){
+            for(int j=0;j<r;j++){
+                if(i>=j){
+                    System.out.print("*");
+                }
+                else
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
