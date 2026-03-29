65cbd5dcb0ab3aaa4e8d4ed2f02206ec3266c842
Felcia A <jeyakumararul91@gmail.com>
2026-03-29 14:38:58 +0530

    Add files via upload

---
+++ pattern8.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class pattern8 {
+    public static void main(String[]args){
+    Scanner s=new Scanner(System.in);
+    int n=s.nextInt();
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(j<i)
+            System.out.print(" ");
+            else 
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+    }
+}
